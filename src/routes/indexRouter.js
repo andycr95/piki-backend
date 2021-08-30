77ae9370e95f6aeb60c6bdcf34adb49fe9ac60c7
kjrichard <kevinjairrichardrosero@gmail.com>
2021-08-29 20:41:13 -0500

    marge

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -5,6 +5,14 @@ const router = Router();
 
 router.use('/usuarios',require('./userRouter'));
 router.use('/conductores',require('./driverRouter'));
+router.use("/conductores", require('./driverRouter'));
+router.use("/lineas", require('./lineRouter'));
+router.use("/tipos", require('./typeRouter'));
+router.use("/clientes", require('./clientRouter'));
+router.use("/patios", require('./yardRouter'));
+router.use("/contenedores", require('./containerRouter'));
+router.use("/turnos", require('./shiftRouter'));
+router.use("/asignar_contenedores", require('./asignContainerRouter'));
 
 
 module.exports = router;
