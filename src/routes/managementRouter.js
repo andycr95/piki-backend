7f8aa8f570f87a475366c8a14b50d243e43d7b44
Henry Stivent <henrystiven7@hotmail.com>
2021-09-15 22:47:31 -0500

    Methods getShift and saveShift implemented

--- src/routes/managementRouter.js
+++ src/routes/managementRouter.js
@@ -3,5 +3,7 @@ const router = Router();
 const managementController = require('../controllers/managementController');
 
 router.get('/', managementController.getInfo);
+router.get('/:id', managementController.getShiftById);
+router.put('/save/:id', managementController.saveShift);
 
 module.exports = router;
