b36223c2f2876a6e0346d50f1aa85faf670c8cac
kjrichard <kevinjairrichardrosero@gmail.com>
2021-10-14 19:25:03 -0500

    company full

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -14,7 +14,6 @@ companyCtrl.newCompany = async ( req, res ) => {
     try {
         let newCompany  = req.body; 
         const foundCompany = await db.company.findOne({ where:{ name: newCompany.name } });
-        console.log(foundCompany);
         if(foundCompany) return res.status( 404 ).json({ menssage: 'La empresa ya existe.'});
         newCompany.name = newCompany.name.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
         db.company.create( newCompany );
